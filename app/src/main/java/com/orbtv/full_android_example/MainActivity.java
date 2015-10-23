b02099ccb314c16ffc525e4380060deb89abe9fb
mikepierre <mikeypierre84@yahoo.com>
2015-10-22 23:01:05 -0400

    Added Dialogs and calendar
    Added Dialogs and calendar to project.

--- app/src/main/java/com/orbtv/full_android_example/MainActivity.java
+++ app/src/main/java/com/orbtv/full_android_example/MainActivity.java
@@ -1,20 +1,27 @@
 package com.orbtv.full_android_example;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
 
 import java.text.DateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
     private EditText game3ScoreEditText;
     private TextView date;
     private TextView seriesTotal;
+    private Button dateBtn;
+
+    private int month;
+    private int day;
+    private int year;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,8 +44,15 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setUpViews();
 
+        Calendar calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+
+
         // put todays date on the screen
-        Date today = new Date();
+        Date today = calendar.getTime();
+
         DateFormat df = DateFormat.getDateInstance(DateFormat.MEDIUM);
         String cs = df.format(today);
         date.setText(cs);
@@ -47,16 +66,56 @@ public class MainActivity extends AppCompatActivity {
         int score2 = Integer.parseInt(rawScore);
         rawScore = game3ScoreEditText.getText().toString();
         int score3 = Integer.parseInt(rawScore);
+
+        Log.d("EnterScores", "I hear the save btn");
+        DatePickerDialog.OnDateSetListener datePickerListener;
+        datePickerListener = new DatePickerDialog.OnDateSetListener(){
+            @Override
+            public void onDateSet(DatePicker view, int yearOfYear, int monthOfYear, int dayOfMonth){
+
+                year = yearOfYear;
+                month = monthOfYear;
+                day = dayOfMonth;
+
+                Calendar cal = new GregorianCalendar(year, month, day);
+                Date dateOfGames = cal.getTime();
+                DateFormat df = DateFormat.getDateInstance(DateFormat.MEDIUM);
+                String cs = df.format(dateOfGames);
+                date.setText(cs);
+
+            }
+        };
+
+
+        DatePickerDialog dpd = new DatePickerDialog(this,datePickerListener, year, month, day);
+        dpd.show();
+
         if(isValid(score1) && isValid(score2) && isValid(score3)){
             // make object of bowling scores
             BowlingScores bowlingScores;
-            bowlingScores = new BowlingScores(score1, score2, score3, new Date());
+            Date dateOfGames = new Date(year, month, day);
+            bowlingScores = new BowlingScores(score1, score2, score3, dateOfGames);
             seriesTotal.setText(" " + bowlingScores.calculateSeriesScore());
             // get aplication object to add bowling score object to array list
-            MyBowlingScoresApplication app = (MyBowlingScoresApplication) getApplication();
+            MyBowlingScoresApplication app =
+                    (MyBowlingScoresApplication) getApplication();
             app.addBowlingScores(bowlingScores);
         } else {
             // pop up dialog indicating data is invalid.
+            // builder pattern.
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Invalid Scores").setMessage("Bowling scores cannot ne greater than 300").setCancelable(false)
+                    .setPositiveButton("Ok",
+
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.cancel();
+                                }
+                            }
+                    );
+            AlertDialog alert = builder.create();
+            alert.show();
         }
 
     }
